cd9d9a32e89a3e14f7e5d03efaad5a1aaf29c6b7
germe-deb <dpkg.luci@protonmail.com>
2025-08-24 20:01:17 -0300

    first commit

--- game/conf.lua
+++ game/conf.lua
@@ -37,13 +37,13 @@ function love.conf(t)
 
   t.window.title          = product_config["PRODUCT_NAME"]
   t.window.icon           = nil
-  t.window.width          = 1920
-  t.window.height         = 1080
+  t.window.width          = 800
+  t.window.height         = 600
   t.window.borderless     = false
-  t.window.resizable      = false
+  t.window.resizable      = true
   t.window.minwidth       = 1
   t.window.minheight      = 1
-  t.window.fullscreen     = false
+  t.window.fullscreen     = true
   t.window.fullscreentype = "desktop"
   t.window.vsync          = 1
   t.window.msaa           = 0
