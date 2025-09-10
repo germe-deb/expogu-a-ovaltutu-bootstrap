432cdc935b794c9389e0aa16bf946dda0172b11c
germe-deb <dpkg.luci@protonmail.com>
2025-09-10 08:18:18 -0300

    changed config

--- game/conf.lua
+++ game/conf.lua
@@ -41,8 +41,8 @@ function love.conf(t)
   t.window.height         = 682
   t.window.borderless     = false
   t.window.resizable      = true
-  t.window.minwidth       = 1
-  t.window.minheight      = 1
+  t.window.minwidth       = 350
+  t.window.minheight      = 580
   t.window.fullscreen     = false
   t.window.fullscreentype = "desktop"
   t.window.vsync          = 1
@@ -65,8 +65,8 @@ function love.conf(t)
   t.modules.keyboard      = true
   t.modules.math          = true
   t.modules.mouse         = true
-  t.modules.physics       = true
-  t.modules.sound         = true
+  t.modules.physics       = false
+  t.modules.sound         = false
   t.modules.system        = true
   t.modules.thread        = true
   t.modules.timer         = true
